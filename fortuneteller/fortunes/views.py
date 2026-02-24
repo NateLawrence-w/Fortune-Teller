f434e6d5698c1d23aa65467e1245a6e52298359b
NateLawrence-w <Natewolf676@gmail.com>
2026-02-23 22:14:27 -0500

    Add fortune model, view, and debugging notes; update fortune template

--- fortuneteller/fortunes/views.py
+++ fortuneteller/fortunes/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+import random 
 
 # Create your views here.
 def  fortune(request):
-    return render(request, 'fortunes/fortune.html')
+    fortune = random.choice(fortuneList)
+    context = {
+        "fortune": fortune
+    }
+    return render(request, 'fortunes/fortune.html', context)
+
+fortuneList = ["All will go well,", "Be weary of the wolf within your mind but dont be lead astray from the wolf eaither", "This is your momment dont be afraid.", "The greatest risk is not taking one.", "Dont let pride be your fall, but let courage be your guide."]
+
